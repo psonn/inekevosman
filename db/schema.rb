df8172f2f9bcc66d3ae950392825fc27ac1a0553
Peter Sonneveld <peter_tjuuh@hotmail.com>
2014-08-05 00:33:10 +0200

    Added name and username to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804010539) do
+ActiveRecord::Schema.define(version: 20140804222436) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20140804010539) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
