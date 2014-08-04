469746f268c91023b3ba295c82a0dbeb7736cd89
Peter Sonneveld <peter_tjuuh@hotmail.com>
2014-08-05 00:39:51 +0200

    Added validations and further customized username views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
 	mount_uploader :image, ImageUploader
+
+  validates :title, :body, presence: { :message => "Mag niet leeg zijn." }
+  validates :title, :body, length:   { minimum: 2, :message => "Moet minimaal 2 tekens zijn." }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :articles
+
+  validates :first_name, :last_name, :username, :email, :presence => { :message => "Mag niet leeg zijn." }
+  validates :username, uniqueness: { case_sensitive: false, message: "Is al in gebruik." }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX, message: "Is ongeldig." },
+                    uniqueness: { case_sensitive: false, message: "Is al in gebruik." }
+  validates :password, length: { minimum: 6, message: "Moet minimaal 6 tekens zijn." }, :on => :create
+  validates :password_confirmation, presence: { :message => "Wachtwoorden komen niet overeen." }, :on => :create
+  validates :password, confirmation: { :message => "Wachtwoorden komen niet overeen." }
+
 end
