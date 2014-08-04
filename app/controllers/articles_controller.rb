b63d1c6b301fc8eb7f97c9af20e881c4e55c28a4
Peter Sonneveld <peter_tjuuh@hotmail.com>
2014-08-05 00:13:23 +0200

    Display articles in reverse chronological order

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.all.order("created_at DESC")
   end
 
   def show
