7724e33648c7b6031cea66179983fe4bb04e8425
Peter Sonneveld <peter_tjuuh@hotmail.com>
2014-08-05 03:26:38 +0200

    Added users overview page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Ineke5::Application.routes.draw do
     get "sign_up", to: "devise/registrations#new"
   end
 
+  get 'users' => 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
